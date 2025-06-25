c60c58f3c6157e3a2f6060fb26a75e5cd53c2ea4
ti3rrr <punch.doublekill@gmail.com>
2025-06-25 20:23:28 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,14 +11,13 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use('/js', express.static(path.join(__dirname, '../public/js')));
 app.use('/img', express.static(path.join(__dirname, '../public/img')));
 
+app.get("/test", (req, res) => {
+  res.send("API is working!");
+});
+
 app.use('/', route);
 
-// Set Views
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// Listen on port 3000
-const port = 3000;
-app.listen(port, function () {
-  console.log('Bubble node app running on port ' + port);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Bubble node app running on port " + port);
 });
