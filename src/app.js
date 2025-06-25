5598bff2f61da4b716e51394cf2bfe26053eceb3
ti3rrr <punch.doublekill@gmail.com>
2025-06-25 20:53:14 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use('/', route);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Bubble node app running on port ' + port);
 });
